c8d8cd67d49194201bd162b9c2fcf6221a16177b
Raiska8 <87840244+Raiska8@users.noreply.github.com>
2022-02-26 22:46:02 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./index.html";
+import "./goods.html";
+
+import "./css/bootstrap-grid.min.css";
+import "./css/swiper-bundle.min.css";
+import "./css/style.css";
+
+import "./js/getGoods.ts";
+import "./js/search.ts";
+import "./js/cart.ts";
